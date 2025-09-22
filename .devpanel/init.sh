af6f0d1ab6dccc3badde8da932d4ff8aee6f6208
piyuesh23 <piyuesh.kumar@qed42.com>
2025-09-22 15:36:08 +0530

    Enabling site install with init.

--- .devpanel/init.sh
+++ .devpanel/init.sh
@@ -66,19 +66,19 @@ fi
 
 #== Install Drupal.
 echo
-# if [ -z "$(drush status --field=db-status)" ]; then
-#   echo 'Install Drupal.'
-#   time drush -n si
+if [ -z "$(drush status --field=db-status)" ]; then
+  echo 'Install Drupal.'
+  time drush -n si
 
-#   echo
-#   echo 'Tell Automatic Updates about patches.'
-#   drush -n cset --input-format=yaml package_manager.settings additional_trusted_composer_plugins '["cweagans/composer-patches"]'
-#   drush -n cset --input-format=yaml package_manager.settings additional_known_files_in_project_root '["patches.json", "patches.lock.json"]'
-#   time drush ev '\Drupal::moduleHandler()->invoke("automatic_updates", "modules_installed", [[], FALSE])'
-# else
-  # echo 'Update database.'
-  # time drush -n updb
-# fi
+  echo
+  echo 'Tell Automatic Updates about patches.'
+  drush -n cset --input-format=yaml package_manager.settings additional_trusted_composer_plugins '["cweagans/composer-patches"]'
+  drush -n cset --input-format=yaml package_manager.settings additional_known_files_in_project_root '["patches.json", "patches.lock.json"]'
+  time drush ev '\Drupal::moduleHandler()->invoke("automatic_updates", "modules_installed", [[], FALSE])'
+else
+  echo 'Update database.'
+  time drush -n updb
+fi
 
 #== Warm up caches.
 echo
