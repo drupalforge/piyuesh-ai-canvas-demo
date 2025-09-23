a937349f175bc030a36ceb035bc007e46224920e
piyuesh23 <piyuesh.kumar@qed42.com>
2025-09-23 17:03:23 +0530

    U[pdating the base to use drupal cms.

--- .devpanel/composer_setup.sh
+++ .devpanel/composer_setup.sh
@@ -3,9 +3,9 @@ set -eu -o pipefail
 cd $APP_ROOT
 
 # Create required composer.json and composer.lock files
-time composer create-project -n --no-plugins --no-install drupal/recommended-project
-cp -r recommended-project/* ./
-rm -rf recommended-project patches.lock.json
+time composer create-project -n --no-plugins --no-install drupal/cms
+cp -r cms/* ./
+rm -rf cms patches.lock.json
 
 # Add Drush and Composer Patches.
 composer require -n --no-plugins --no-update drush/drush cweagans/composer-patches:^2@beta
